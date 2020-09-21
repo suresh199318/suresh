89e1725a9aac9d670574fdc0151f62640b5ba00c
root <root@ip-172-31-44-50.ec2.internal>
2020-09-21 04:37:19 +0000

    shell_script

---
+++ curl_shell.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+curl  --header "X-Vault-Token: s.4dIFt7PzEmZjI1JXUr6usHHd" \
+			--request POST \
+			--data @payload.json \
+              http://http://54.242.74.133:8200/v1/auth/approle/role/applicaa
