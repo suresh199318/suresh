7c6a5781874c846864d761e5ef6e1fa6c824f8ca
suresh199318 <46858927+suresh199318@users.noreply.github.com>
2020-09-21 11:00:31 +0530

    Update curl_shell.sh

--- curl_shell.sh
+++ curl_shell.sh
@@ -3,4 +3,4 @@
 curl  --header "X-Vault-Token: s.4dIFt7PzEmZjI1JXUr6usHHd" \
 			--request POST \
 			--data @payload.json \
-              http://54.242.74.133:8200/v1/auth/approle/role/test-role
+              http://54.242.74.133:8200/v1/auth/approle/role/test-role1
